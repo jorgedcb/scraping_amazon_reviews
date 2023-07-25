b6be4c6203de6aba0f9cb9fed806a397e71c2a18
Jorge Eduardo Castilla Bonet <jecb2001@gmail.com>
2023-07-24 21:13:59 -0500

    First commit

--- amazon/settings.py
+++ amazon/settings.py
@@ -7,25 +7,19 @@ NEWSPIDER_MODULE = 'amazon.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
-SCRAPEOPS_API_KEY = 'YOUR_API_KEY'
+SCRAPEOPS_API_KEY = '3e6dec3d-e5ab-4fe8-ba37-c884f1a4a21a'
 
 SCRAPEOPS_PROXY_ENABLED = True
 # SCRAPEOPS_PROXY_SETTINGS = {'country': 'us'}
 
 # Add In The ScrapeOps Monitoring Extension
-EXTENSIONS = {
-'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
-}
+# EXTENSIONS = {
+# 'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+# }
 
 LOG_LEVEL = 'INFO'
 
 DOWNLOADER_MIDDLEWARES = {
-
-    ## ScrapeOps Monitor
-    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
-    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
-    
-    ## Proxy Middleware
     'scrapeops_scrapy_proxy_sdk.scrapeops_scrapy_proxy_sdk.ScrapeOpsScrapyProxySdk': 725,
 }
 
--- amazon/spiders/amazon_reviews.py
+++ amazon/spiders/amazon_reviews.py
@@ -1,38 +1,51 @@
 import scrapy
 from urllib.parse import urljoin
+import logging
 
 class AmazonReviewsSpider(scrapy.Spider):
+
+    logging.basicConfig(filename='scraper.log', level=logging.DEBUG,
+                        format= '%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     name = "amazon_reviews"
+
+    handle_httpstatus_list = [404]
 
     custom_settings = {
         'FEEDS': { 'data/%(name)s_%(time)s.csv': { 'format': 'csv',}}
         }
 
     def start_requests(self):
-        asin_list = ['B09G9FPHY6']
+        asin_list = ['B09RD7XM9X']
         for asin in asin_list:
-            amazon_reviews_url = f'https://www.amazon.com/product-reviews/{asin}/'
-            yield scrapy.Request(url=amazon_reviews_url, callback=self.parse_reviews, meta={'asin': asin, 'retry_count': 0})
+            amazon_reviews_url = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_getr_d_paging_btm_next_1?pageNumber=1"
+            yield scrapy.Request(url=amazon_reviews_url, callback=self.parse_reviews, meta={'asin': asin, 'retry_count': 0, 'page_count': 1})
 
 
     def parse_reviews(self, response):
+        
         asin = response.meta['asin']
         retry_count = response.meta['retry_count']
+        page_count = response.meta['page_count']
 
-        next_page_relative_url = response.css(".a-pagination .a-last>a::attr(href)").get()
-        if next_page_relative_url is not None:
-            retry_count = 0
-            next_page = urljoin('https://www.amazon.com/', next_page_relative_url)
-            yield scrapy.Request(url=next_page, callback=self.parse_reviews, meta={'asin': asin, 'retry_count': retry_count})
-
-        ## Adding this retry_count here so we retry any amazon js rendered review pages
-        elif retry_count < 3:
-            retry_count = retry_count+1
-            yield scrapy.Request(url=response.url, callback=self.parse_reviews, dont_filter=True, meta={'asin': asin, 'retry_count': retry_count})
-
-
-        ## Parse Product Reviews
         review_elements = response.css("#cm_cr-review_list div.review")
+        if len(review_elements) > 0 and page_count <= 19:
+            print(f"sussecsfull: {response.url}")
+            retry_count = 0
+            page_count += 1
+            if page_count <= 10:
+                next_page = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_getr_d_paging_btm_next_{page_count}?pageNumber={page_count}"
+            else:
+                next_page = f"https://www.amazon.com/product-reviews/{asin}/ref=cm_cr_getr_d_paging_btm_next_{page_count-10}?pageNumber={page_count-10}&sortBy=recent"
+            yield scrapy.Request(url=next_page, callback=self.parse_reviews, meta={'asin': asin, 'retry_count': retry_count, 'page_count': page_count})
+
+        elif retry_count < 3 and page_count <= 20:
+            print(f"retrying {response.url} {retry_count+1}/3")
+            if response.status != 404:
+                with open(f"responses/{asin}_page_{page_count}_retry_{retry_count}.html", "w", encoding='utf-8') as file:
+                    file.write(response.text)
+            retry_count = retry_count+1
+            yield scrapy.Request(url=response.url, callback=self.parse_reviews, dont_filter=True, meta={'asin': asin, 'retry_count': retry_count, 'page_count': page_count})
+
         for review_element in review_elements:
             yield {
                     "asin": asin,
